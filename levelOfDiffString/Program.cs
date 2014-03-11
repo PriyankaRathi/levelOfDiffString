39046d15a6e4c1dffbe1f46206960451d711d8b0
PriyankaRathi <priyankajmantri@gmail.com>
2014-03-12 11:44:16 +1300

    Initial commit

---
+++ levelOfDiffString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace levelOfDiffString
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+            string B = Console.ReadLine();
+            var S = from i in Enumerable.Range(0, A.Length)
+                    from j in Enumerable.Range(0, A.Length - i + 1)
+                    where j >= 1
+                    select A.Substring(i, j);
+                              
+            var T = from i in Enumerable.Range(0, B.Length)
+                    from j in Enumerable.Range(0, B.Length - i + 1)
+                    where j >= 1
+                    select B.Substring(i, j);
+           
+            var P =S.Except(T).Union(T.Except(S));
+
+            Console.Write(P.Count());
+
+            //Console.ReadKey();
+        }
+
+       
+    }
+}
